a424aa4db9fd2d116acc2fe8c6400b70d7f540fc
Cesar Alexandre <cesaralexandreprofissional@gmail.com>
2024-05-25 15:33:25 +0100

    backend frontend - connection

--- frontend/src/pages/News.js
+++ frontend/src/pages/News.js
@@ -1,10 +1,31 @@
-import React from 'react';
+// src/News.js
+import React, { useEffect, useState } from 'react';
+import { fetchNews } from '../api';
 
 const News = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetchNews()
+      .then(response => {
+        setNews(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching news:', error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>News</h1>
-      <p>This is the News page.</p>
+      <ul>
+        {news.map(article => (
+          <li key={article._id}>
+            <strong>{article.title}</strong>
+            <p>{article.content}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
