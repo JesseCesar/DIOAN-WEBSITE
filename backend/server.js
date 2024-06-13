79092a5d6664b603cb77f14bf0bb3ea872e1d459
Mojela Ratsita <mojelaratsita1@gmail.com>
2024-06-13 19:54:19 +0200

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
@@ -5,6 +6,8 @@ import newsRoutes from './routes/news.routes.js';
 import { connectToMongoDB } from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+
+const __dirname = path.resolve()
 
 dotenv.config();
 const app = express();
@@ -22,6 +25,10 @@ app.get('/', (req, res) => {
 
 app.use("/api/user", userRoutes)
 app.use("/api/news", newsRoutes)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))})
 
 app.listen(PORT, () => {
   connectToMongoDB()
