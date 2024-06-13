cdd62e1a05113abdfe108f1963088a260ac7609a
Mojela Ratsita <mojelaratsita1@gmail.com>
2024-06-13 21:36:52 +0200

    deployment fail fix 1.0.5

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,40 @@
-import path from 'path';
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.routes.js';
-import newsRoutes from './routes/news.routes.js';
-import { connectToMongoDB } from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-const __dirname = path.resolve()
+import userRoutes from './routes/user.routes.js';
+import newsRoutes from './routes/news.routes.js';
+import { connectToMongoDB } from './db/connectToMongoDB.js';
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'http://localhost:3000' // Adjust as necessary
 }));
 
-const PORT = process.env.PORT || 5000;
+// API routes
+app.use("/api/user", userRoutes);
+app.use("/api/news", newsRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!!!!!');
-})
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.use("/api/user", userRoutes)
-app.use("/api/news", newsRoutes)
+// Catch-all route to serve React frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
-
-app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))})
-
+// Start the server and connect to the database
 app.listen(PORT, () => {
-  connectToMongoDB()
-  console.log(`Server is running on port  ${PORT}`)})
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
